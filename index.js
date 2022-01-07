d983ea5f1eb7b94671d4561b5ae110ae6de09ba2
Alexey Onufriev <aleksonufriev@gmail.com>
2022-01-08 00:19:33 +0200

    fixes port

--- index.js
+++ index.js
@@ -3,9 +3,10 @@ const express = require('express');
 const app = express();
 
 const dir = path.join(__dirname, 'public');
+const PORT = process.env.PORT || 3001
 
 app.use(express.static(dir));
 
-app.listen(3000, function () {
+app.listen(PORT, function () {
     console.log('Listening on http://localhost:3000/');
 });
